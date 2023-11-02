5885a752dffc15a204d16c28e4cc48c2d92ffa29
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2023-11-02 16:59:11 +0000

    commit

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -5,12 +5,25 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Question", menuName = "Quiz Question", order = 0)]
 public class QuestionSO : ScriptableObject 
 {
+    
     [TextArea(2, 6)]
     [SerializeField] string question = "Enter new question here";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswerIndex;
 
     public string GetQuestion()
     {
         return question;
+    }
+
+    public string GetAnswer(int index)
+    {
+        return answers[index];
+    }
+
+    public int GetCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
     }
 
 }
