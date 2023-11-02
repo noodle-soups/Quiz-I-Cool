cd44f39ba1784555c1e983466c6f08131e5ef756
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2023-11-02 16:14:16 +0000

    commit

---
+++ Assets/Scripts/QuestionSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Question", menuName = "Quiz Question", order = 0)]
+public class QuestionSO : ScriptableObject 
+{
+    [TextArea(2, 6)]
+    [SerializeField] string question = "Enter new question here";
+}
