f47e3b74d864fd38092b895f6c0bed87dcc7664a
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2023-11-08 11:40:16 +0000

    Update Timer.cs

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -7,12 +7,19 @@ public class Timer : MonoBehaviour
 
     [SerializeField] float timeToCompleteQuestion = 30f;
     [SerializeField] float timeToShowCorrectAnswer = 10f;
+    public bool loadNextQuestion;
     public bool isAnsweringQuestion = false;
+    public float fillFraction;
     float timerValue;
 
     void Update()
     {
         UpdateTimer();
+    }
+
+    public void CancelTimer()
+    {
+        timerValue = 0;
     }
 
     void UpdateTimer()
@@ -21,7 +28,11 @@ public class Timer : MonoBehaviour
 
         if(isAnsweringQuestion)
         {
-            if(timerValue <= 0)
+            if(timerValue > 0)
+            {
+                fillFraction = timerValue / timeToCompleteQuestion;
+            }
+            else
             {
                 isAnsweringQuestion = false;
                 timerValue = timeToShowCorrectAnswer;
@@ -29,14 +40,19 @@ public class Timer : MonoBehaviour
         }
         else
         {
-            if(timerValue <= 0)
+            if(timerValue > 0)
+            {
+                fillFraction = timerValue / timeToShowCorrectAnswer;
+            }
+            else
             {
                 isAnsweringQuestion = true;
                 timerValue = timeToCompleteQuestion;
+                loadNextQuestion = true;
             }
         }
 
-        Debug.Log(timerValue);
+        Debug.Log(isAnsweringQuestion + ": " + timerValue + " = " + fillFraction);
     }
 
 }
