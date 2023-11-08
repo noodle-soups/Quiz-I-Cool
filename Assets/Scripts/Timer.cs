9215cec562cb7506eb7543dc8647fc2cac5fbfda
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2023-11-08 11:23:41 +0000

    commit

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    [SerializeField] float timeToCompleteQuestion = 30f;
+    [SerializeField] float timeToShowCorrectAnswer = 10f;
+    public bool isAnsweringQuestion = false;
+    float timerValue;
+
     void Update()
     {
-        
+        UpdateTimer();
     }
+
+    void UpdateTimer()
+    {
+        timerValue -= Time.deltaTime;
+
+        if(isAnsweringQuestion)
+        {
+            if(timerValue <= 0)
+            {
+                isAnsweringQuestion = false;
+                timerValue = timeToShowCorrectAnswer;
+            }
+        }
+        else
+        {
+            if(timerValue <= 0)
+            {
+                isAnsweringQuestion = true;
+                timerValue = timeToCompleteQuestion;
+            }
+        }
+
+        Debug.Log(timerValue);
+    }
+
 }
